3d21b8d65394c6a5860d4d21443f135af9cd3acc
famasoon <famasoon@gmail.com>
2019-04-23 22:06:12 +0900

    Add support infixExpression

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -64,7 +64,7 @@ func (l *Lexer) NextToken() token.Token {
 	case '<':
 		tok = newToken(token.LT, l.ch)
 	case '>':
-		tok = newToken(token.RT, l.ch)
+		tok = newToken(token.GT, l.ch)
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
--- token/token.go
+++ token/token.go
@@ -22,7 +22,7 @@ const (
 	SLASH    = "/"
 
 	LT = "<"
-	RT = ">"
+	GT = ">"
 
 	COMMA     = ","
 	SEMICOLON = ";"
